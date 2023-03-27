1d6fa73b38e0bf53bd81570e50d6cefc0f98a4bb
Joao Taves <joaotaves@MacBook-Air-de-Joao-3.local>
2023-03-27 11:41:25 -0300

    refactor class and break down in more steps for ease of debugging

--- NN.py
+++ NN.py
@@ -1,21 +1,13 @@
 import numpy as np
 import ipdb
 import matplotlib.pyplot as plt
-from utils import visualize_convergence, relu, sigmoid, tanh
+from utils import visualize_convergence, relu, sigmoid, tanh, relu_backward, sigmoid_backward, tanh_backward
 
 class NeuralNetwork:
     def __init__(self, sizes, activation, learning_rate=0.1, epochs=1000):
         self.learning_rate = learning_rate
         self.epochs = epochs
         self.sizes = sizes
-
-        if activation == 'sigmoid':
-            self.activation_function = sigmoid
-        elif activation == 'tanh':
-            self.activation_function = tanh
-        elif activation == 'relu':
-            self.activation_function = relu
-
 
         self.params = self.initialize_params(sizes)
 
@@ -31,61 +23,120 @@ class NeuralNetwork:
 
         return params
     
+    def linear_forward(self, A, W, b):
+        Z = (np.dot(W, A) + b)
+        cache = (A, W, b)
+        return Z, cache
+
+    def linear_activation_forward(self, A_prev, W, b, activation='relu'):
+        if activation == 'relu':
+            Z, linear_cache = self.linear_forward(A_prev, W, b)
+            A, activation_cache = relu(Z)
+
+        elif activation == 'sigmoid':
+            Z, linear_cache = self.linear_forward(A_prev, W, b)
+            A, activation_cache = sigmoid(Z)
+
+        elif activation == 'tanh':
+            Z, linear_cache = self.linear_forward(A_prev, W, b)
+            A, activation_cache = tanh(Z)
+
+        cache = (linear_cache, activation_cache)
+
+        return A, cache
+
     def forward_propagation(self, X):
         params = self.params
         cache = self.cache
+        caches = []
+
+        A = X
         L = len(params) // 2
 
-        cache['A0'] = X
-        #ipdb.set_trace()
-        for i in range(1, L+1):
-            cache['Z' + str(i)] = np.dot(params['W' + str(i)], cache['A' + str(i-1)]) + params['b' + str(i)]
-            cache['A' + str(i)] = self.activation_function(cache['Z' + str(i)])
+        for i in range(1, L):
+            A, cache = self.linear_activation_forward(A, params['W' + str(i)], params['b' + str(i)], activation='relu')
+            caches.append(cache)
 
-        return cache['A' + str(i)]
-    
+        AL, cache = self.linear_activation_forward(A, params['W' + str(L)], params['b' + str(L)], activation='sigmoid')
+        caches.append(cache)
+        return AL, caches
+
     def compute_cost(self, y_hat, y):
-        m = y.shape[1]
-        cost = -1/m * np.sum(y.T * np.log(y_hat) + (1 - y.T) * np.log(1 - y_hat))
+        m = y.shape[0]
+
+        cost = -1/m * np.sum(y * np.log(y_hat) + (1 - y) * np.log(1 - y_hat), axis=1)
         return cost
+
     
-    def backward_propagation(self, y_hat, y):
-        params = self.params
-        cache = self.cache
+    def linear_backward(self, dZ, cache):
+        A_prev, W, b = cache
+        m = A_prev.shape[1]
+
+        dW = 1/m * np.dot(dZ, A_prev.T)
+        db = 1/m * np.sum(dZ, axis=1, keepdims=True)
+        dA_prev = np.dot(W.T, dZ)
+
+        return dA_prev, dW, db
+
+    def linear_activation_backward(self, dA, cache, activation='relu'):
+        
+        linear_cache, activation_cache = cache
+
+        if activation == 'relu':
+            dZ = relu_backward(dA, activation_cache)
+            dA_prev, dW, db = self.linear_backward(dZ, linear_cache)
+        
+        elif activation == 'sigmoid':
+            dZ = sigmoid_backward(dA, activation_cache)
+            dA_prev, dW, db = self.linear_backward(dZ, linear_cache)
+            
+        elif activation == 'tanh':
+            dZ = tanh_backward(dA, activation_cache)
+            dA_prev, dW, db = self.linear_backward(dZ, linear_cache)
+
+        return dA_prev, dW, db
+
+    
+    def backward_propagation(self, AL, y, caches):
         grads = {}
+        L = len(caches)
+        m = AL.shape[1]
+        y = y.reshape(AL.shape)
 
-        L = len(params) // 2
+        dAL = - (np.divide(y, AL) - np.divide(1 - y, 1 - AL))
 
-        m = y.shape[1]
+        dA_prev_temp, dW_temp, db_temp = self.linear_activation_backward(dAL, caches[L-1], 'sigmoid')
+        grads[f"dA{L-1}"] = dA_prev_temp
+        grads[f"dW{L}"] = dW_temp
+        grads[f"db{L}"] = db_temp
 
-        grads['dZ' + str(L)] = y_hat - y
-        for i in reversed(range(1, L+1)):
-            if i == L:
-                dZ = grads['dZ' + str(i)]
-                A_prev = cache['A' + str(i-1)].T
-            else:
-                dZ = np.dot(dA, params['W' + str(i+1)].T) * (cache['A' + str(i)] > 0)
-                A_prev = cache['A' + str(i-1)].T
-            grads['dW' + str(i)] = np.dot(A_prev, dZ) / m
-            grads['db' + str(i)] = np.sum(dZ, axis=0, keepdims=True) / m
-            dA = np.dot(dZ, params['W' + str(i)].T)
+        for l in reversed(range(L-1)):
+            current_cache = caches[l]
+            dA_prev_temp, dW_temp, db_temp = self.linear_activation_backward(dA_prev_temp, current_cache, 'relu')
+            grads[f"dA{l}"] = dA_prev_temp
+            grads[f"dW{l+1}"] = dW_temp
+            grads[f"db{l+1}"] = db_temp
 
         return grads
-    
+  
     def update_params(self, grads):
         params = self.params
+
+        L = len(params) // 2
 
         for i in range(1, len(params) // 2 + 1):
             params['W' + str(i)] = params['W' + str(i)] - self.learning_rate * grads['dW' + str(i)]
             params['b' + str(i)] = params['b' + str(i)] - self.learning_rate * grads['db' + str(i)]
 
+        return params
+
     def fit(self, X, y):
         costs = []
 
         for i in range(self.epochs):
-            y_hat = self.forward_propagation(X)
+            y_hat, caches = self.forward_propagation(X)
             cost = self.compute_cost(y_hat, y)
-            grads = self.backward_propagation(y_hat, y.T)
+            grads = self.backward_propagation(y_hat, y.T, caches)
             self.update_params(grads)
 
             if i % 100 == 0:
@@ -106,14 +157,15 @@ class NeuralNetwork:
         return self.params
         
 def main():
-    from sklearn.datasets import load_diabetes
+    #from sklearn.datasets import load_diabetes
     from sklearn.model_selection import train_test_split
     from sklearn.preprocessing import StandardScaler
     import pandas as pd
     
-    data = load_diabetes()
-    X = data.data
-    y = data.target.reshape(-1, 1)
+    data = pd.read_csv('diabetes.csv')
+    #ipdb.set_trace()
+    X = data.drop('Outcome', axis=1).values
+    y = data['Outcome'].values.reshape(-1, 1)
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
@@ -121,12 +173,12 @@ def main():
     X_train = scaler.fit_transform(X_train)
     X_test = scaler.transform(X_test)
 
-    nn = NeuralNetwork([353, 10, 5, 2, 1], activation='relu', learning_rate=0.01, epochs=1000)
+    nn = NeuralNetwork([614, 8, 5, 2, 1], activation='relu', learning_rate=0.01, epochs=1000)
 
     #ipdb.set_trace()
     params = nn.get_params()
-    nn.fit(X_train, y_train.T)
-    print(nn.score(X_test, y_test.T))
+    nn.fit(X_train, y_train)
+    print(nn.score(X_test, y_test))
 
 if __name__ == '__main__':
     main()
--- utils.py
+++ utils.py
@@ -1,24 +1,106 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import h5py
 
-def relu(x):
-    return np.maximum(0, x)
+import numpy as np
 
-def sigmoid(x):
-    return 1 / (1 + np.exp(-x))
-
-def tanh(x):
-    return np.tanh(x)
-
-
-def linear_forward(A, W, b):
-    Z = np.dot(W,A) + b
+def sigmoid(Z):
+    """
+    Implements the sigmoid activation in numpy
     
-    cache = (A, W, b)
+    Arguments:
+    Z -- numpy array of any shape
     
-    return Z, cache
+    Returns:
+    A -- output of sigmoid(z), same shape as Z
+    cache -- returns Z as well, useful during backpropagation
+    """
+    
+    A = 1/(1+np.exp(-Z))
+    cache = Z
+    
+    return A, cache
 
+def relu(Z):
+    """
+    Implement the RELU function.
+
+    Arguments:
+    Z -- Output of the linear layer, of any shape
+
+    Returns:
+    A -- Post-activation parameter, of the same shape as Z
+    cache -- a python dictionary containing "A" ; stored for computing the backward pass efficiently
+    """
+    
+    A = np.maximum(0,Z)
+    
+    assert(A.shape == Z.shape)
+    
+    cache = Z 
+    return A, cache
+
+
+def relu_backward(dA, cache):
+    """
+    Implement the backward propagation for a single RELU unit.
+
+    Arguments:
+    dA -- post-activation gradient, of any shape
+    cache -- 'Z' where we store for computing backward propagation efficiently
+
+    Returns:
+    dZ -- Gradient of the cost with respect to Z
+    """
+    
+    Z = cache
+    dZ = np.array(dA, copy=True) # just converting dz to a correct object.
+    
+    # When z <= 0, you should set dz to 0 as well. 
+    dZ[Z <= 0] = 0
+    
+    assert (dZ.shape == Z.shape)
+    
+    return dZ
+
+def sigmoid_backward(dA, cache):
+    """
+    Implement the backward propagation for a single SIGMOID unit.
+
+    Arguments:
+    dA -- post-activation gradient, of any shape
+    cache -- 'Z' where we store for computing backward propagation efficiently
+
+    Returns:
+    dZ -- Gradient of the cost with respect to Z
+    """
+    
+    Z = cache
+    
+    s = 1/(1+np.exp(-Z))
+    dZ = dA * s * (1-s)
+    
+    assert (dZ.shape == Z.shape)
+    
+    return dZ
+
+def tanh_backward(dA, Z):
+    s = np.tanh(Z)
+    dZ = dA * (1 - s**2)
+    
+    return dZ
+
+def tanh(Z):
+    A = np.tanh(Z)
+    cache = Z
+    
+    return A, cache
+
+def load_data():
+    #load h5 file data
+    dataset = h5py.File('datasets/train_catvnoncat.h5', "r")
+    return dataset
 
 def visualize_convergence(costs, learning_rate, epochs):
     plt.plot(costs)
